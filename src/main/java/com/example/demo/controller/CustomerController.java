96db0ae9432bdc7bce73430e33701cc2d416c923
ryanm0034 <ryan111828@gmail.com>
2024-10-30 18:38:19 -0400

    feat: add get, update, delete customer api

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/customers")
@@ -17,5 +19,30 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto customerDto) {
         CustomerDto savedCustomer = customerService.createCustomer(customerDto);
         return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("id") Long customerId) {
+        CustomerDto customerDto = customerService.getCustomerById(customerId);
+        return ResponseEntity.ok(customerDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> getAllCustomers() {
+        List<CustomerDto> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<CustomerDto> updateCustomer(@PathVariable("id") Long customerId,
+                                                      @RequestBody CustomerDto updatedCustomer) {
+        CustomerDto customerDto = customerService.updateCustomer(customerId, updatedCustomer);
+        return ResponseEntity.ok(customerDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long customerId) {
+        customerService.deleteCustomer(customerId);
+        return ResponseEntity.ok("Customer deleted successfully!");
     }
 }
--- src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.service.impl;
 
 import com.example.demo.dto.CustomerDto;
 import com.example.demo.entity.Customer;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.mapper.CustomerMapper;
 import com.example.demo.repository.CustomerRepository;
 import com.example.demo.service.CustomerService;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -29,21 +31,46 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerDto getCustomerById(Long customerId) {
-        return null;
+        logger.info("Fetching customer with ID: {}", customerId);
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> {
+                    logger.error("Customer not found with ID: {}", customerId);
+                    return new ResourceNotFoundException("Customer does not exist with given id: " + customerId);
+                });
+
+        logger.info("Successfully fetched customer with ID: {}", customerId);
+        return CustomerMapper.mapToCustomerDto(customer);
     }
 
     @Override
     public List<CustomerDto> getAllCustomers() {
-        return List.of();
+        logger.info("Fetching all customers");
+        List<Customer> customers = customerRepository.findAll();
+        logger.info("Successfully fetched all customers");
+        return customers.stream().map(CustomerMapper::mapToCustomerDto).collect(Collectors.toList());
     }
 
     @Override
     public CustomerDto updateCustomer(Long customerId, CustomerDto updatedCustomer) {
-        return null;
+        Customer customer = customerRepository.findById(customerId).orElseThrow(
+                () -> new ResourceNotFoundException("Customer does not exist with given id: " + customerId));
+
+        customer.setFirstName(updatedCustomer.getFirstName());
+        customer.setMiddleName(updatedCustomer.getMiddleName());
+        customer.setLastName(updatedCustomer.getLastName());
+        customer.setEmailAddress(updatedCustomer.getEmailAddress());
+        customer.setPhoneNumber(updatedCustomer.getPhoneNumber());
+
+        Customer savedCustomer = customerRepository.save(customer);
+
+        return CustomerMapper.mapToCustomerDto(savedCustomer);
     }
 
     @Override
     public void deleteCustomer(Long customerId) {
+        customerRepository.findById(customerId).orElseThrow(
+                () -> new ResourceNotFoundException("Customer does not exist with given id: " + customerId));
 
+        customerRepository.deleteById(customerId);
     }
 }
